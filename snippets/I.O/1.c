0653e1d795f0c2ae4280a97f99fcc8e31c8fc480
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-02-16 17:29:00 +0100

    Create 1.c

---
+++ snippets/I.O/1.c
@@ -0,0 +1,36 @@
+ // =============== //
+ //quits if Q is entered
+
+int c1=0;
+while(c1 != EOF && c1 != 'Q') {
+  c1=getchar();
+  printf("%d\n",c1);
+ }
+
+/*
+        In:
+        abc
+
+        Out:
+        97
+        98
+        99
+*/
+
+// =============== //
+
+// =============== //
+
+  int C=0;//C=getchar();
+    while(C != EOF){
+  
+    C=getchar();
+    putchar(C);
+    printf("%c\n",C);
+    printf("%d\n",C);
+  }
+  
+  // =============== //
+  
+  
+  
