3fc8f2a3fe0b32be6e3c85f46f93a8b7fa1636d0
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-02-16 18:31:59 +0100

    Update awzs.c

--- snippets/loops/XtremelyCompact/awzs.c
+++ snippets/loops/XtremelyCompact/awzs.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-//one liner of loops.
 int main() {
-  // for(int i='a';i<'z';putchar(i),i++);       // a,b,c...
-  // for(int i='a';i<'z';printf("%c\n",i),i++); // a,b,c...
-  // for(int i='A';i<'Z';putchar(i),i++);       // A,B,C...
-  for(int i='a';i<'z';printf("%d\n",i),i++);    // 97,98,99..
+  // for loop                                      //  outputs                |data type|
+  // for(int i='a';i<='z';putchar(i),i++);          // a,b,c...x,y,z          |  char   |
+  // for(int i='a';i<='z';printf("%c\n",i),i++);    // a,b,c...x,y,z          |  char   |
+  // for(int i='A';i<='Z';putchar(i),i++);          // A,B,C...X,Y,Z          |  char   |
+  for(int i='a';i<='z';printf("%d\n",i),i++);     // 97,98,99..120,121,122    |  ascii  |
+  //for(int i='a';i<='z';printf("%x\n",i),i++);       // 61,62,63 ,78,79,7a   |  hex    |
+
+ //// for(int i='a';i<'z';printf("%d\n",i),F(i < 5),i++);
+// for(int i='a';i<='z'; printf("%c\n",i),i++);
   return 0;
 }
