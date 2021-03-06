e40baca2b0b9e88c3cf2e486112578e54a5958f8
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-03-06 12:06:41 +0100

    Create Lines.c

---
+++ Templates/Lines.c
@@ -0,0 +1,23 @@
+
+#define NL printf("\n");
+#define SEP printf("-----------");
+
+int a=0;
+int b=0;
+int c=0;
+
+
+printf("%d\n",a);
+scanf("%d",&a);
+
+int n=0; 
+scanf("%d",&n);
+
+for (int i=0; i < n; ++i)
+{
+  printf("%d\n",i);
+}
+ 
+ // same
+ // for (int i=0; i < n; printf("%d\n",i), ++i);
+
